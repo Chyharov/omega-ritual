f4167188e64b7c5d9b2fe38f3de979528bd87416
myusername <chyharov@gmail.com>
2023-04-22 22:16:25 +0300

    added modal window for CarParkList

--- src/components/RitualCarParkList/RitualCarParkList.jsx
+++ src/components/RitualCarParkList/RitualCarParkList.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import s from '../RitualCarParkList/RitualCarParkList.module.scss';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
@@ -8,20 +9,28 @@ import carVW4 from '../../images/whiteVolksWagen/photo_4_2023-04-20_20-12-55.jpg
 import carVW5 from '../../images/whiteVolksWagen/photo_6_2023-04-20_20-12-55.jpg';
 import carVW6 from '../../images/whiteVolksWagen/photo_2_2023-04-20_20-12-55.jpg';
 
-import carWM from '../../images/whiteMercedes/photo_5_2023-04-20_20-13-44.jpg';
-import carWM2 from '../../images/whiteMercedes/photo_7_2023-04-20_20-13-44.jpg';
-import carWM3 from '../../images/whiteMercedes/photo_12_2023-04-20_20-13-44.jpg';
-import carWM4 from '../../images/whiteMercedes/photo_10_2023-04-20_20-13-44.jpg';
-
-import carBM from '../../images/blackMercedes/photo_1_2023-04-20_20-13-21.jpg';
-import carBM2 from '../../images/blackMercedes/photo_2_2023-04-20_20-13-21.jpg';
-import carBM3 from '../../images/blackMercedes/photo_14_2023-04-20_20-13-44.jpg';
-import carBM4 from '../../images/blackMercedes/photo_10_2023-04-20_20-13-21.jpg';
-import carBM5 from '../../images/blackMercedes/photo_4_2023-04-20_20-13-21.jpg';
-import carBM6 from '../../images/blackMercedes/photo_4_2023-04-20_20-13-44.jpg';
-import carBM7 from '../../images/blackMercedes/photo_9_2023-04-20_20-13-44.jpg';
+const Modal = ({ image, closeModal }) => {
+  return (
+    <div className={s.modal}>
+      <div className={s.modalContent}>
+        <span className={s.close} onClick={closeModal}>&times;</span>
+        <img src={image} alt="modal" />
+      </div>
+    </div>
+  );
+};
 
 const RitualCarParkList = () => {
+  const [modalImage, setModalImage] = useState(null);
+
+  const openModal = (image) => {
+    setModalImage(image);
+  };
+
+  const closeModal = () => {
+    setModalImage(null);
+  };
+
   return (
     <>
     <h3 className={s.ritualCarPark__title}>Ритуальний транспорт</h3>
@@ -30,8 +39,7 @@ const RitualCarParkList = () => {
     <p className={s.ritualCarPark__description}>У ритуальному бюро похоронний дім «Омега» ви можете замовити у будь-яку точку Києва та Київської області один із чотирьох видів катафалків. Ціна таких послуг залежить від класу ритуального транспорту та його місткості. Це може бути як бюджетний економ варіант, так і катафалк VIP-рівня.</p>
     <section className={s.ritualCarPark}>
           
-      <div className="container">
-        <div className={s.ritualCarPark__container}>
+    <div className={s.ritualCarPark__container}>
           <ul>
             <li className={s.ritualCarParkList}>
                 <p className={s.ritualCarParkList__item}>Економ клас</p>
@@ -42,80 +50,28 @@ const RitualCarParkList = () => {
                     visibleSlides={1}
                 >
                     <Slider>
-                    <Slide index={0}><img src={carVW} alt="ritualCar" /></Slide>
-                    <Slide index={1}><img src={carVW2} alt="ritualCar" /></Slide>
-                    <Slide index={2}><img src={carVW3} alt="ritualCar" /></Slide>
-                    <Slide index={3}><img src={carVW4} alt="ritualCar" /></Slide>
-                    <Slide index={4}><img src={carVW5} alt="ritualCar" /></Slide>
-                    <Slide index={4}><img src={carVW6} alt="ritualCar" /></Slide>
+                      <Slide index={0}><img src={carVW} alt="ritualCar" onClick={() => openModal(carVW)} /></Slide>
+                      <Slide index={1}><img src={carVW2} alt="ritualCar" onClick={() => openModal(carVW2)} /></Slide>
+                      <Slide index={2}><img src={carVW3} alt="ritualCar" onClick={() => openModal(carVW3)} /></Slide>
+                      <Slide index={3}><img src={carVW4} alt="ritualCar" onClick={() => openModal(carVW4)} /></Slide>
+                      <Slide index={4}><img src={carVW5} alt="ritualCar" onClick={() => openModal(carVW5)} /></Slide>
+                      <Slide index={5}><img src={carVW6} alt="ritualCar" onClick={() => openModal(carVW6)} /></Slide>
                     </Slider>
 
                     <p className={s.ritualCarParkList__name}>Volkswagen Т-5 Білий</p>
                     <p className={s.ritualCarParkList__description}>(2 - 5 пасажирів)</p>
 
                     <div className={s.carouselButtonCenter}>
-                        <ButtonBack className={s.carouselButton}>Назад</ButtonBack>
-                        <ButtonNext className={s.carouselButton}>Вперед</ButtonNext>
+                      <ButtonBack className={s.carouselButton}>Назад</ButtonBack>
+                      <ButtonNext className={s.carouselButton}>Вперед</ButtonNext>
                     </div>
                 </CarouselProvider>
             </li>
-
-            <li className={s.ritualCarParkList}>
-                <p className={s.ritualCarParkList__item}>Середній клас</p>
-                <CarouselProvider
-                    naturalSlideWidth={280}
-                    naturalSlideHeight={210}
-                    totalSlides={4}
-                    visibleSlides={1}
-                >
-                    <Slider>
-                    <Slide index={0}><img src={carWM} alt="ritualCar" /></Slide>
-                    <Slide index={1}><img src={carWM2} alt="ritualCar" /></Slide>
-                    <Slide index={2}><img src={carWM3} alt="ritualCar" /></Slide>
-                    <Slide index={3}><img src={carWM4} alt="ritualCar" /></Slide>
-
-                    </Slider>
-
-                    <p className={s.ritualCarParkList__name}>Mercedes Benz Sprinter Білий</p>
-                    <p className={s.ritualCarParkList__description}>(14 - 18 пасажирів)</p>
-
-                    <div className={s.carouselButtonCenter}>
-                        <ButtonBack className={s.carouselButton}>Назад</ButtonBack>
-                        <ButtonNext className={s.carouselButton}>Вперед</ButtonNext>
-                    </div>
-                </CarouselProvider>
-            </li>
-
-            <li className={s.ritualCarParkList}>
-                <p className={s.ritualCarParkList__item}>VIP клас</p>
-                <CarouselProvider
-                    naturalSlideWidth={280}
-                    naturalSlideHeight={210}
-                    totalSlides={7}
-                    visibleSlides={1}
-                >
-                    <Slider>
-                    <Slide index={0}><img src={carBM} alt="ritualCar" /></Slide>
-                    <Slide index={1}><img src={carBM2} alt="ritualCar" /></Slide>
-                    <Slide index={2}><img src={carBM3} alt="ritualCar" /></Slide>
-                    <Slide index={3}><img src={carBM4} alt="ritualCar" /></Slide>
-                    <Slide index={4}><img src={carBM5} alt="ritualCar" /></Slide>
-                    <Slide index={4}><img src={carBM6} alt="ritualCar" /></Slide>
-                    <Slide index={4}><img src={carBM7} alt="ritualCar" /></Slide>
-                    </Slider>
-
-                    <p className={s.ritualCarParkList__name}>Mercedes Benz Sprinter Чорний</p>
-                    <p className={s.ritualCarParkList__description}>(14 - 18 пасажирів)</p>
-
-                    <div className={s.carouselButtonCenter}>
-                        <ButtonBack className={s.carouselButton}>Назад</ButtonBack>
-                        <ButtonNext className={s.carouselButton}>Вперед</ButtonNext>
-                    </div>
-                </CarouselProvider>
-            </li>
+            {modalImage && (
+            <Modal image={modalImage} closeModal={closeModal} />
+            )}
           </ul>
         </div>
-      </div>
     </section>
     </>
   );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
-
 import s from '../Header/Header.module.scss';
 import Logo from '../Logo/Logo';
 import NavBar from 'components/NavBar/NavBar';
 
 
 const Header = () => {
-  const [isActive, setIsActive] = useState(false);
-  const isMobile = useMediaQuery({ query: '(max-width: 767.99px)' });
-  const isTablet = useMediaQuery({ query: '(min-width: 768px)' });
-
-  const handleScroll = () => {
-    if (isMobile && window.scrollY > 200 && !isActive) {
-      setIsActive(true);
-      return;
-    }
-    if (isTablet && window.scrollY > 80 && !isActive) {
-      setIsActive(true);
-      return;
-    }
-    setIsActive(false);
-  };
-
-  useEffect(() => {
-    document.addEventListener('scroll', handleScroll);
-    return () => document.removeEventListener('scroll', handleScroll);
-  });
-
   return (
-    <header className={isActive === true ? s['header--active'] : s.header}>
+    <header className={s.header}>
       <div className={'container ' + s.header__container}>
         <Logo />
       </div>
